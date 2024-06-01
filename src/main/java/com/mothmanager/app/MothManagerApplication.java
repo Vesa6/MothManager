9d47a7ba15b07a019a28905b589e0f7f37cc1809
Vesa6 <83303692+Vesa6@users.noreply.github.com>
2024-06-01 23:47:45 +0300

    spring init

---
+++ src/main/java/com/mothmanager/app/MothManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mothmanager.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MothManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MothManagerApplication.class, args);
+	}
+
+}
